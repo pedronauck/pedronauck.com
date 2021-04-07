42aec13fd8de8603efbde2f96d30fa11161cd278
Pedro Nauck <pedronauck@gmail.com>
2021-04-07 02:53:07 -0300

    feat: add favicon and gtm

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,105 @@
 import { ColorModeScript } from '@chakra-ui/react'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+
+import { GTMScriptHead, GTMScriptBody } from 'components/GTMScript'
 import theme from 'styles/theme'
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <GTMScriptHead />
+          <title>Pedro Nauck</title>
+          <link
+            rel="apple-touch-icon"
+            sizes="57x57"
+            href="/favicon/apple-icon-57x57.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="60x60"
+            href="/favicon/apple-icon-60x60.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="72x72"
+            href="/favicon/apple-icon-72x72.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="/favicon/apple-icon-76x76.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="114x114"
+            href="/favicon/apple-icon-114x114.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="120x120"
+            href="/favicon/apple-icon-120x120.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="144x144"
+            href="/favicon/apple-icon-144x144.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/favicon/apple-icon-152x152.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon/apple-icon-180x180.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="/favicon/android-icon-192x192.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="96x96"
+            href="/favicon/favicon-96x96.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+          <meta name="theme-color" content="#ffffff" />
+          <meta property="og:locale" content="pt-br" />
+          <meta property="og:url" content="https://www.pedronauck.com" />
+          <meta property="og:title" content="Pedro Nauck" />
+          <meta property="og:site_name" content="Pedro Nauck" />
+          <meta
+            property="og:description"
+            content="#BatePapoDev toda às quartas as 19h30. Se inscreva!"
+          />
+          <meta property="og:image" content="/meta-image.jpg" />
+          <meta property="og:image:type" content="image/jpeg" />
+          <meta property="og:image:width" content="1024" />
+          <meta property="og:image:height" content="512" />
+          <meta property="og:type" content="website" />
+        </Head>
         <body>
+          <GTMScriptBody />
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,9 @@
-import Head from 'next/head'
-
 import Content from 'components/Content'
 import Info from 'components/Info'
 
 export default function Home() {
   return (
     <div className="flex flex-col-reverse sm:flex-row sm:justify-between">
-      <Head>
-        <title>@pedronauck</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Content />
       <Info />
     </div>
