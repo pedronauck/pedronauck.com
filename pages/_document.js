d53edfd0ec573b1951670e74e7125117f6497983
Pedro Nauck <pedronauck@gmail.com>
2021-04-19 19:12:57 -0300

    fix: import segment tag

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { ColorModeScript } from '@chakra-ui/react'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 
 import { GTMScriptHead, GTMScriptBody } from 'components/GTMScript'
-import { SegmentTag } from 'components/SegmentTag'
+import SegmentTag from 'components/SegmentTag'
 import theme from 'styles/theme'
 
 export default class Document extends NextDocument {
